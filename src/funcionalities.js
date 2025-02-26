187a7a1752ac16347d315314a152427b535c5212
ian <ianmassaccesi.dev@hotmail.com>
2025-02-26 03:01:25 -0300

    tes3l

--- src/funcionalities.js
+++ src/funcionalities.js
@@ -15,7 +15,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function createParticle() {
         const particle = document.createElement("img");
-        particle.src = "ACCAMA-WEB/src/assets/cannabis-leaf-svgrepo-com.svg"; // Ruta de la imagen SVG
+        
+
+        particle.src = "src": "https://ianmassaccesi.github.io/accama/src/assets/cannabis-leaf-svgrepo-com.svg"; // Ruta de la imagen SVG
         particle.style.position = "absolute";
         particle.style.width = "30px";
         particle.style.height = "30px";
